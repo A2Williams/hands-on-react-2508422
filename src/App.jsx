e3e32a2b8339f616ed97a96f7eead272341bb8ce
Aaron Williams <aaronchristoph.williams@ontariotechu.net>
2022-12-24 02:49:56 +0000

    finished 01 and 02 exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Support from './components/Support';
+import ListCast from './components/ListCast';
+import Modals from './components/Modals';
+import Nav from './components/Nav';
+
 function App() {
-  const name = 'StarGazers'
+  const [cast, setCast ] = useState([]);
+  let [memberInfo, setMemberInfo] = useState(null);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+  
+  // solution to wrapping problem
+  function handleMembers(info) {
+    setMemberInfo(info > Number(cast.length) - 1 ? cast[0] :
+      info <  0 ? cast[Number(cast.length) - 1] : cast[info]);
+  }
+
   return (
+    <>
+    <Nav cast={cast} onChoice={(info) => {setMemberInfo(info)}}/>
     <div className="container">
-      <article>
         <hgroup>
           <img src="images/group.svg" alt="Stargazers group"/>
-          <h1>Meet the <i style={{color:"steelblue"}}>{name}</i></h1>
+          <h1>Meet the StarGazers</h1>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={()=> alert('hi')}>click</button>
+          <ListCast cast={ cast } onChoice={ (info) => {setMemberInfo(info)}}/>
+          { memberInfo &&
+            <Modals member={memberInfo}
+                    handleClose={() => {setMemberInfo(null)}}
+                    handleMember={(info) => {handleMembers(info)}} />}
+          <Support />
         </hgroup>
-      </article>
     </div>
+    </>
   )
 }
 export default App
---
+++ src/components/Modals.jsx
@@ -0,0 +1,32 @@
+export default ({member, handleClose, handleMember}) => {
+  return (
+     <dialog id="modal-member" open>
+      <article>
+        <a href="#close"
+        aria-label="Close"
+        className="close"
+        data-target="modal-member"
+        onClick={handleClose}>
+        </a>
+        <hgroup>
+          <div style={{
+            display: "flex",
+            gap: "1rem",
+          }}>
+            <img style={{ width: '200px'}} src={`images/${member.slug}.svg`} alt={member.name} />
+            <hgroup>
+              <h1>{member.name}</h1>
+              <p>{member.bio}</p>
+              <hgroup>
+              <a className="outline" href="#" role="button" 
+                onClick={() => {handleMember(Number(member.id) + 1)}}>{`next`}</a>
+              <a className="outline" href="#" role="button" 
+                onClick={() => {handleMember(Number(member.id) - 1)}}>{`previous`}</a>
+              </hgroup>
+            </hgroup>
+          </div>
+        </hgroup>
+      </article>
+     </dialog>
+  )
+}
